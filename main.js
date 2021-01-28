875b2a08961bad773364fb7d75756320e65f7c86
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-01-27 20:56:10 -0500

    Operadores_comparacion_27_01_2021

---
+++ main.js
@@ -0,0 +1,50 @@
+let a=50;
+let b=100;
+console.log("let a="+a);
+console.log("let b="+b+"\n\n");
+
+console.log("comparacion == : ");
+console.log(+a==b); 
+ 
+ a=50;
+ b=100;
+
+ console.log("comparacion != ");
+console.log(a!=b);
+
+
+a=50;
+b=100;
+
+console.log("comparacion === ");
+console.log(a===b);
+
+a=50;
+b=100;
+
+console.log("comparacion !== ");
+console.log(a!==b);
+
+a=50;
+b=100; 
+
+console.log("comparacion > ");
+console.log(a>b);
+
+a=50; 
+b=100;
+
+console.log("comparacion < ");
+console.log(a<b);
+
+a=50;
+b=100;
+
+console.log("comparacion >= ");
+console.log(a>=b);
+
+a=50;
+b=100;
+
+console.log("comparacion <= ");
+console.log(a<=b);
